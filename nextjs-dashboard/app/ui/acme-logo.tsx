bc45c86d07c136b92a79225e0d76b0483296435b
lcarrasco95 <locava95@gmail.com>
2025-01-22 19:27:54 -0600

    Initial commit

---
+++ nextjs-dashboard/app/ui/acme-logo.tsx
@@ -0,0 +1,13 @@
+import { GlobeAltIcon } from '@heroicons/react/24/outline';
+import { lusitana } from '@/app/ui/fonts';
+
+export default function AcmeLogo() {
+  return (
+    <div
+      className={`${lusitana.className}  text-xl text-gray-800 md:text-3xl md:leading-normal`}
+    >
+      <GlobeAltIcon className="h-12 w-12 rotate-[15deg]" />
+      <p className="text-[44px]">Acme</p>
+    </div>
+  );
+}
