2d14ef1d5bda17f6070701865f01073e523d92f6
lcarrasco95 <locava95@gmail.com>
2025-01-27 13:11:29 -0600

    Adding Search

--- nextjs-dashboard/app/ui/dashboard/nav-links.tsx
+++ nextjs-dashboard/app/ui/dashboard/nav-links.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import {
   UserGroupIcon,
   HomeIcon,
@@ -7,8 +8,7 @@ import {
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import clsx from 'clsx';
-// Map of links to display in the side navigation.
-// Depending on the size of the application, this would be stored in a database.
+
 const links = [
   { name: 'Home', href: '/dashboard', icon: HomeIcon },
   {
@@ -26,22 +26,22 @@ export default function NavLinks() {
       {links.map((link) => {
         const LinkIcon = link.icon;
         return (
-          <Link
-            key={link.name}
-            href={link.href}
-            className={clsx(
-              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
-              {
-                'bg-sky-100 text-blue-600': pathname === link.href,
-              },
-            )}
-            className="flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3"
-          >
-            <LinkIcon className="w-6" />
-            <p className="hidden md:block">{link.name}</p>
-          </Link>
+          <Link key={link.name} href={link.href}>
+  <div
+    className={clsx(
+      'flex h-[48px] grow items-center justify-center gap-2 rounded-md p-3 text-sm font-medium md:flex-none md:justify-start md:p-2 md:px-3',
+      pathname === link.href
+        ? 'bg-sky-100 text-blue-600'
+        : 'bg-gray-50 hover:bg-sky-100 hover:text-blue-600',
+    )}
+  >
+    <LinkIcon className="w-6" />
+    <p className="hidden md:block">{link.name}</p>
+  </div>
+</Link>
         );
       })}
     </>
   );
 }
+// Comentario para ver si hace push
